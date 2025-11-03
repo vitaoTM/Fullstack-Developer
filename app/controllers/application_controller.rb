e20694e095d5fe5e69176745a3e2b43ae722a3b5
vitaotm <vitor.grosskopf@gmail.com>
2025-11-03 14:25:59 -0300

    Configure routes and role-based login redirects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   #
   include Pundit::Authorization
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authonticate_user!
   allow_browser versions: :modern
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
@@ -12,6 +13,14 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :full_name, :avatar_image, :avatar_url ])
     devise_parameter_sanitizer.permit(:account_update, keys: [ :full_name, :avatar_url, :avatar_image ])
+  end
+
+  def after_sign_in_path_for(resource)
+    if  resource.admin?
+      admin_dashboard_path
+    else
+      profile_path
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,23 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
 
+  namespace :admin do
+    get "dashboard", to: "dashboard#show"
+
+    resources :users
+
+    patch "users/:id/toggle_role", to: "users#toggle_role", as: toggle_user_role
+
+    resources :user_imports, only: [ :new, :crete, :show ]
+  end
+
+  resources :profile, only: [ :show, :edit, :update, :destroy ]
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "admin/dashboard#show"
+  get "up" => "rails/health#show", as: :rails_health_check
 end
