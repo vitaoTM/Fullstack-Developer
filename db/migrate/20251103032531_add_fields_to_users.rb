bc7687392973640a8ed37645397a67945276ddad
vitaotm <vitor.grosskopf@gmail.com>
2025-11-03 00:28:25 -0300

    First commit
    - Set up devise User
    - add gems
    - install active_storage

---
+++ db/migrate/20251103032531_add_fields_to_users.rb
@@ -0,0 +1,6 @@
+class AddFieldsToUsers < ActiveRecord::Migration[8.0]
+  def change
+    add_column :users, :full_name, :string
+    add_column :users, :role, :integer, default: 0, null: false
+  end
+end
